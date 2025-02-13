bd0626bca5d4ea2aff27bc9bfad0811c663a65aa
Ilia Shebanov <ilya.shebanoff@gmail.com>
2025-02-13 15:18:01 +0200

    add tests folder

--- tests/test_automation_practice_form.py
+++ tests/test_automation_practice_form.py
@@ -15,8 +15,8 @@ def test_complete_and_submit_form():
     browser.element('#userNumber').type('9990006666')
 
     browser.element('#dateOfBirthInput').perform(command.js.click)
-    browser.element('.react-datepicker__month-select').perform(command.js.click).element('option[value="7"]').perform(command.js.click)
-    browser.element('.react-datepicker__year-select').perform(command.js.click).element('option[value="1997"]').perform(command.js.click)
+    browser.element('.react-datepicker__month-select').perform(command.js.click).all('option').element_by(have.text('July')).click()
+    browser.element('.react-datepicker__year-select').perform(command.js.click).all('option').element_by(have.text('1997')).click()
     browser.element('.react-datepicker__day--003').perform(command.js.click)
 
     browser.element('#subjectsInput').type('biology').press_enter()
@@ -25,7 +25,7 @@ def test_complete_and_submit_form():
     browser.element('#hobbies-checkbox-1').perform(command.js.click)
     browser.element('#hobbies-checkbox-3').perform(command.js.click)
 
-    browser.element('#uploadPicture').send_keys(os.path.abspath('images/mayk-vazovski-s-litsom-salli-8.jpg'))
+    browser.element('#uploadPicture').send_keys(os.path.abspath('../images/mayk-vazovski-s-litsom-salli-8.jpg'))
 
     browser.element('#currentAddress').type('Sadovaya, 14')
 
@@ -39,7 +39,7 @@ def test_complete_and_submit_form():
     browser.element('.table').should(have.text('fedor.bubnov_test@gmail.com'))
     browser.element('.table').should(have.text('Male'))
     browser.element('.table').should(have.text('9990006666'))
-    browser.element('.table').should(have.text('03 February,2025'))
+    browser.element('.table').should(have.text('03 July,1997'))
     browser.element('.table').should(have.text('Biology, Chemistry'))
     browser.element('.table').should(have.text('Sports, Music'))
     browser.element('.table').should(have.text('mayk-vazovski-s-litsom-salli-8.jpg'))
